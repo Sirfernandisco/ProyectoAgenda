d5c9673a1dc4253e112b4a98c0615f3066679c8b
CarlosVerd <34572410+CarlosVerd@users.noreply.github.com>
2018-06-04 15:06:06 +0200

    asddd

---
+++ PorFinAgenda/src/main/java/spring/services/Service.java
@@ -0,0 +1,55 @@
+package spring.services;
+
+import java.util.ArrayList;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import spring.datos.Datos;
+import spring.model.Empleados;
+
+//@Service
+@Transactional
+public class Service implements IService{
+
+		@Autowired
+		private Datos datos;
+		
+		/*
+		public UserServiceImpl(){
+			
+		}
+		
+		public UserServiceImpl(UserDAO userDAO) {
+			super();
+			this.userDAO = userDAO;
+		}
+		*/
+
+
+		@Override
+		public ArrayList<Empleados> mostrarEmpleados() {
+
+			return datos.mostrarEmpleados();
+		}
+
+//		@Override
+//		public User get(int id) {
+//			return userDAO.get(id);
+//		}
+//
+//		@Override
+//		public void saveOrUpdate(User user) {
+//			userDAO.saveOrUpdate(user);
+//
+//		}
+//
+//		@Override
+//		public void delete(int id) {
+//			userDAO.delete(id);
+//
+//		}
+
+
+}
