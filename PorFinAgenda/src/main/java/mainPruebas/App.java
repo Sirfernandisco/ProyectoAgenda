a8627126794c62d14cfa2361fe977be283788c09
Sirfernandisco <capitancannabis_fer@hotmail.com>
2018-06-05 10:27:20 +0200

    fer

---
+++ PorFinAgenda/src/main/java/mainPruebas/App.java
@@ -0,0 +1,16 @@
+package mainPruebas;
+
+import spring.datos.Datos;
+import spring.datos.Idatos;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Idatos dat= new Datos();
+		
+		System.out.println(dat.mostrarEmpleados());
+
+	}
+
+}
