8d19a3905afbfbb26a332d0f4e2532d2ea3c6554
HeIIhound <orlandopereira1233@hotmail.com>
2018-06-06 16:48:58 +0200

    asd

--- PorFinAgenda/src/main/java/spring/control/InicioController.java
+++ PorFinAgenda/src/main/java/spring/control/InicioController.java
@@ -35,7 +35,6 @@ public class InicioController {
 		return "index";
 	}
 
-	// añadido para mostrar empleados falta modificar
 	@RequestMapping("/mostrar")
 	public ModelAndView handleRequest() throws Exception {
 		List<Empleados> listUsers = userService.mostrarEmpleados();
@@ -95,32 +94,5 @@ public class InicioController {
 		userService.borrarEmpleado(id);
 		return new ModelAndView("redirect:/mostrar");		
 	}
-	
-	
-	// @RequestMapping(value = "/addCliente", method = RequestMethod.POST)
-	// public String processSubmit(
-	// @ModelAttribute("cliente") Cliente cliente,
-	// ModelMap model) {
-	// System.out.println("--- metodo addcliente");
-	//
-	//
-	// model.addAttribute("nombre", cliente.getNombre());
-	// model.addAttribute("password", cliente.getPassword());
-	// model.addAttribute("id", cliente.getId());
-	//
-	// //En vez de guardar uno a uno... los guardo de golpe
-	// //En el proximo JSP tendre que sacarlos como cliente.nombre ciente.id,
-	// etc.
-	// System.out.println("--"+cliente);
-	// model.addAttribute(cliente);
-	// return "form2";
-	// }
-
-	// Tambien podria usar
-	/*
-	 * public ModelAndView processSubmit(@ModelAttribute("cliente") Cliente
-	 * cliente, BindingResult result){ ModelAndView mv = new ModelAndView();
-	 * mv.addObject("cliente", cliente); return mv;
-	 */
 
 }
