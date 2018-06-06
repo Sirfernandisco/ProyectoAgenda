7db17695996525a6a9e88c0c4e26f90e3133f11c
Sirfernandisco <capitancannabis_fer@hotmail.com>
2018-06-06 17:20:35 +0200

    fer

--- PorFinAgenda/src/main/java/spring/control/InicioController.java
+++ PorFinAgenda/src/main/java/spring/control/InicioController.java
@@ -109,30 +109,5 @@ public class InicioController {
 	
 	
 	
-	// @RequestMapping(value = "/addCliente", method = RequestMethod.POST)
-	// public String processSubmit(
-	// @ModelAttribute("cliente") Cliente cliente,
-	// ModelMap model) {
-	// System.out.println("--- metodo addcliente");
-	//
-	//
-	// model.addAttribute("nombre", cliente.getNombre());
-	// model.addAttribute("password", cliente.getPassword());
-	// model.addAttribute("id", cliente.getId());
-	//
-	// //En vez de guardar uno a uno... los guardo de golpe
-	// //En el proximo JSP tendre que sacarlos como cliente.nombre ciente.id,
-	// etc.
-	// System.out.println("--"+cliente);
-	// model.addAttribute(cliente);
-	// return "form2";
-	// }
-
-	// Tambien podria usar
-	/*
-	 * public ModelAndView processSubmit(@ModelAttribute("cliente") Cliente
-	 * cliente, BindingResult result){ ModelAndView mv = new ModelAndView();
-	 * mv.addObject("cliente", cliente); return mv;
-	 */
 
 }
