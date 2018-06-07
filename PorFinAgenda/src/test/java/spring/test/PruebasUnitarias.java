8d18e6c0972fc7c02ffc13855c69791664f0079d
Sirfernandisco <capitancannabis_fer@hotmail.com>
2018-06-07 11:05:56 +0200

    fer

--- PorFinAgenda/src/test/java/spring/test/PruebasUnitarias.java
+++ PorFinAgenda/src/test/java/spring/test/PruebasUnitarias.java
@@ -1,9 +1,8 @@
 package spring.test;
 
-
 import static org.junit.Assert.*;
-
-
+import org.apache.commons.logging. Log;
+import org.apache.commons.logging. LogFactory;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -13,38 +12,44 @@ import org.springframework.test.context.junit4.rules.SpringClassRule;
 import org.springframework.test.context.junit4.rules.SpringMethodRule;
 
 import spring.confi.ApplicationContextConfig;
+
 import spring.datos.Idatos;
+import spring.model.Personas;
 
 @ContextConfiguration(classes = (ApplicationContextConfig.class))
 
 public class PruebasUnitarias {
-
-
 	
-	@ClassRule 
-	public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();	
+	static Log log = LogFactory.getLog(PruebasUnitarias.class.getName());
+
+	@ClassRule
+	public static final SpringClassRule SPRING_CLASS_RULE = new SpringClassRule();
 	@Rule
-    public final SpringMethodRule springMethodRule = new SpringMethodRule();
-	
-    private int a;
-    private int b;
-	
+	public final SpringMethodRule springMethodRule = new SpringMethodRule();
+
+	private int a;
+	private int b;
+
 	@Autowired
 	private Idatos datos;
-	
-	
+
 	@Test
-	public void comprobarSuma(){
-		a=3;
-		b=2;
+	public void testSuma() {
+		a = 3;
+		b = 2;
 		assertEquals(5, datos.suma(a, b));
 	}
 	
+	@Test
+	public void testBuscadorEncontrar(){
+		assertEquals("Rodriguez",datos.get("Rodri").getApellido1());
+	}
 	
-	
-	
-	
+	@Test
+	public void testBuscadorNoEncontrar(){
+		log.info("************"+datos.get("")+"***************");
+		assertEquals("",datos.get("").getApellido1());
+		//assertNotEquals(unexpected, actual);
+	}
 
-	
-	
 }
